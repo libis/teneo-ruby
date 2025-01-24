946ef015d6114cfc75469c2d611bfd479d2f5fa3
Kris Dekeyser <kris.dekeyser@kuleuven.be>
2025-01-24 16:38:54 +0100

    Ruby upgrade to 3.4.1

--- start.sh
+++ start.sh
@@ -2,8 +2,22 @@
 
 touch Gemfile
 
-echo "updating gems ..."
-bundle check > /dev/null 2>&1 || bundle install
+case "$BUNDLE_COMMAND" in
+    "")
+        ;;
+    install)
+        echo "installing gems ..."
+        bundle check > /dev/null 2>&1 || bundle install
+        ;;
+    update)
+        echo "updating gems ..."
+        bundle check > /dev/null 2>&1 || bundle update
+        ;;
+    *)
+        echo "unknown command: $BUNDLE_COMMAND"
+        exit 1
+        ;;
+esac
 
 echo "starting application: $@"
 bundle exec "$@"
